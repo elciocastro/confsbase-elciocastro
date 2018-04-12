c3c6982a7564e6eada2a6cdd70ba3a4e91f927b4
root <root@chef-workstation.dexter.com.br>
2018-04-12 17:07:17 -0300

    Configuracao Base de Usuarios, Pacotes e Hosts

---
+++ README.md
@@ -0,0 +1,4 @@
+# confsbase-elciocastro
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name 'confsbase-elciocastro'
+maintainer 'Elcio Castro'
+maintainer_email 'elciocastro89@gmail.com'
+license 'All Rights Reserved'
+description 'Configuracao base'
+long_description 'Configuracao base de usuarios, pacotes e hosts'
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+source_url "https://github.com/elciocastro/confsbase-elciocastro"
